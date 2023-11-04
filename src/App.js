abf5dc4e71f9dc30723a8315edcd9389dc05578d
swaroop-219 <swaroop.mudrady@gmail.com>
2023-11-04 09:20:36 +0530

    NavBar commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Phone from './pages/Phone/Phone';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        
+
+        
+        </Routes>
+      </BrowserRouter>
+      
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +32,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header>*/}
+  
     </div>
+  
   );
 }
+
 
 export default App;
